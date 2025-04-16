7d189a90240f478aa9a093220ce8efe641a8839b
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2025-04-16 10:16:44 -0300

    Update seguimiento.py

--- seguimiento.py
+++ seguimiento.py
@@ -196,11 +196,11 @@ if seleccionados:
         for i, label in enumerate(estados):
             if i < estado_actual:
                 #color = "green"
-                color = "#AAEDEF"
+                color = "#4DB6AC"
                 text_color = "white"
             elif i == estado_actual:
                 #color = "deepskyblue"
-                color = "#EFACAA"
+                color = "#FF8A65"
                 text_color = "white"
             else:
                 color = "lightgray"
